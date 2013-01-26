e0436770f7328d0781ed3c4347098955d0e10fac
Oliver Hoff <oliver@hofff.com>
2013-01-26 03:52:33 +0100

    improve, complete and correct german translation

--- TL_ROOT/system/modules/backboneit_navigation_article/languages/de/tl_module.php
+++ TL_ROOT/system/modules/backboneit_navigation_article/languages/de/tl_module.php
@@ -4,4 +4,7 @@ $GLOBALS['TL_LANG']['tl_module']['bbit_navi_art_legend']
 	= 'Navigationsartikel';
 
 $GLOBALS['TL_LANG']['tl_module']['bbit_navi_art_enable']
-	= array('Navigationsartikel aktivieren', 'Ein angepasstes Navigationstemplate wird für diese Funktion benötigt. Im zugeordneten Artikel wird die Zeichenfolge "{{bbit_navi_art}}" durch die nächste Navigationsebene ersetzt.');
+	= array('Navigationsartikel aktivieren', 'Das "nav_bbit_navi_art" oder ein '
+		. 'selbst angepasstes Navigationstemplate wird für diese Funktion '
+		. 'benötigt. Im zugeordneten Artikel wird das Insert-Tag '
+		. '"{{bbit_navi_art}}" durch die nächste Navigationsebene ersetzt.');
--- TL_ROOT/system/modules/backboneit_navigation_article/languages/de/tl_page.php
+++ TL_ROOT/system/modules/backboneit_navigation_article/languages/de/tl_page.php
@@ -4,18 +4,18 @@ $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_legend']
 	= 'Navigationsartikel';
 
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_articles']
-	= array('Artikelauswahl', 'S = Nicht durchsuchen. C = Artikelcontainer ausgeben. Bitte wählen Sie die Artikel aus, die Sie einfügen möchten.');
+	= array('Artikelauswahl', 'S = Nicht durchsuchen. C = Artikelcontainer ausgeben. Die Artikel, die einfügt werden sollen.');
 
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_module']
-	= array('Modul', 'Bitte wählen Sie das Modul dem ein Artikel zugewiesen werden soll.');
+	= array('Modul', 'Das Modul dem der Artikel zugewiesen werden soll.');
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_article']
-	= array('Bezogener Artikel', 'Bitte wählen Sie den Artikel aus, den Sie einfügen möchten.');
+	= array('Bezogener Artikel', 'Der Artikel der eingefügt werden soll.');
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_cssID']
-	= array('CSS-ID/Klasse', 'Hier können Sie eine ID und beliebig viele Klassen eingeben.');
+	= array('CSS-ID/Klasse', 'Die zu verwendende ID des Artikels und zusätzlich hinzuzufügende CSS Klassen.');
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_nosearch_short']
 	= array('S', 'Schließt dieses Artikelmodul von der Contao-Suche aus.');
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_container_short']
-	= array('C', 'Gibt zusätzlich den Artikelcontainer aus anstatt nur die Inhaltselemente des Artikels.');
+	= array('C', 'Gibt zusätzlich den Artikelcontainer aus und nicht nur die Inhaltselemente des Artikels.');
 
 $GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_errDuplicate']
-	= 'Sie können nur ein Artikel je Modul auswählen.';
+	= 'Jedem Modul kann jeweils nur ein Artikel zugewiesen werden.';
