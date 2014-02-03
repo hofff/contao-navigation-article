1b64f6bc1c1b0e455d4943a5097506c83327b611
Oliver Hoff <oliver@hofff.com>
2014-02-03 12:46:29 +0100

    fix lineendings

--- TL_ROOT/system/modules/backboneit_navigation_article/NavigationArticle.php
+++ TL_ROOT/system/modules/backboneit_navigation_article/NavigationArticle.php
@@ -1,7 +1,7 @@
 <?php
 
 class NavigationArticle extends Frontend {
-	
+
 	public function hook_bbit_navi_item($objNavi, &$arrPage) {
 		$arrArticle = $this->getNavigationArticle($objNavi->id, $arrPage['id']);
 		$arrArticle && $arrPage['bbit_navi_art'] = IncludeArticleUtils::generateArticle(
@@ -12,43 +12,43 @@ class NavigationArticle extends Frontend {
 			$arrArticle['cssID']
 		);
 	}
-	
+
 	protected $arrArticles;
-	
+
 	protected function getNavigationArticle($intModuleID, $intPageID) {
 		if(!isset($this->arrArticles[$intModuleID])) {
 			$this->arrArticles[$intModuleID] = $this->fetchNavigationArticlesForModule($intModuleID);
 		}
 		return $this->arrArticles[$intModuleID][$intPageID];
 	}
-	
+
 	protected function fetchNavigationArticlesForModule($intModuleID) {
 		$objArticles = Database::getInstance()->prepare(
 			'SELECT	*
 			FROM	tl_bbit_navi_art
 			WHERE	module = ?'
 		)->execute($intModuleID);
-		
+
 		$arrArticles = array();
-		
+
 		while($objArticles->next()) {
 			$arrArticles[$objArticles->page] = $objArticles->row();
 		}
-		
+
 		return $arrArticles;
 	}
-	
+
 	protected function __construct() {
 		parent::__construct();
 	}
-	
+
 	private static $objInstance;
-	
+
 	public static function getInstance() {
 		if(!isset(self::$objInstance))
 			self::$objInstance = new self();
-			
+
 		return self::$objInstance;
 	}
-	
+
 }
