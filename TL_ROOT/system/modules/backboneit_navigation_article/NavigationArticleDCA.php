57769c2e47d7910e0e8f7c18d9ce38cd418847ec
Oliver Hoff <oliver@hofff.com>
2014-02-03 12:49:02 +0100

    trailing whitespace

--- TL_ROOT/system/modules/backboneit_navigation_article/NavigationArticleDCA.php
+++ TL_ROOT/system/modules/backboneit_navigation_article/NavigationArticleDCA.php
@@ -1,31 +1,31 @@
 <?php
 
 class NavigationArticleDCA extends Backend {
-	
+
 	public function getModules($objDC) {
 		$objModules = Database::getInstance()->prepare(
 			'SELECT	id, name
 			FROM	tl_module'
 		)->execute();
-	
+
 		$arrModules = array();
 		while($objModules->next()) {
 			$arrModules[$objModules->id] = $objModules->name;
 		}
 		return $arrModules;
 	}
-	
+
 	public function editModule($objDC, $objWidget = null) {
 		if(is_object($objWidget)) { // comes from MCW
 			$intModuleID = $objWidget->value;
 		} else {
 			$intModuleID = $objDC->value;
 		}
-	
+
 		if($intModuleID < 1) {
 			return '';
 		}
-	
+
 		return sprintf(
 			' <a href="contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=%s" title="%s" style="padding-left:3px;">%s</a>',
 			$intArticleID,
@@ -33,7 +33,7 @@ class NavigationArticleDCA extends Backend {
 			$this->generateImage('alias.gif', $GLOBALS['TL_LANG']['tl_page']['editalias'][0], 'style="vertical-align:top;"')
 		);
 	}
-	
+
 	public function loadForPage($arrRows, $objDC) {
 		return Database::getInstance()->prepare(
 			'SELECT	j.*
@@ -44,14 +44,14 @@ class NavigationArticleDCA extends Backend {
 			ORDER BY t.name, m.name'
 		)->execute($objDC->id)->fetchAllAssoc();
 	}
-	
+
 	protected $arrSets;
-	
+
 	public function saveForPage($arrRows, $objDC) {
 		$arrRows = deserialize($arrRows);
 		$intCount = count($arrRows);
 		$this->arrSets[$objDC->id] = array();
-		
+
 		foreach($arrRows as $arrRow) {
 			$arrRow['page'] = $objDC->id;
 			$arrRow['module'] = intval($arrRow['module']);
@@ -62,42 +62,42 @@ class NavigationArticleDCA extends Backend {
 				$intCount--;
 			}
 		}
-		
+
 		if($intCount != count($this->arrSets[$objDC->id])) {
 			throw new Exception($GLOBALS['TL_LANG']['tl_page']['bbit_navi_art_errDuplicate']);
 		}
-		
+
 		return null;
 	}
-	
+
 	public function submitPage($objDC) {
 		if(!isset($this->arrSets[$objDC->id])) {
 			return;
 		}
-		
+
 		$objDB = Database::getInstance();
 		$objDB->prepare(
 			'DELETE FROM tl_bbit_navi_art WHERE page = ?'
 		)->execute($objDC->id);
-		
+
 		if($this->arrSets[$objDC->id]) foreach($this->arrSets[$objDC->id] as $arrSet) {
 			$objDB->prepare(
 				'INSERT INTO tl_bbit_navi_art %s'
 			)->set($arrSet)->execute();
 		}
 	}
-	
+
 	protected function __construct() {
 		parent::__construct();
 	}
-	
+
 	private static $objInstance;
-	
+
 	public static function getInstance() {
 		if(!isset(self::$objInstance))
 			self::$objInstance = new self();
-			
+
 		return self::$objInstance;
 	}
-	
+
 }
--- TL_ROOT/system/modules/backboneit_navigation_article/config/database.sql
+++ TL_ROOT/system/modules/backboneit_navigation_article/config/database.sql
@@ -12,20 +12,20 @@ CREATE TABLE `tl_bbit_navi_art` (
   `page` int(10) unsigned NOT NULL default '0',
   `module` int(10) unsigned NOT NULL default '0',
   `article` int(10) unsigned NOT NULL default '0',
-  
+
   `cssID` varchar(255) NOT NULL default '',
   `nosearch` char(1) NOT NULL default '',
   `container` char(1) NOT NULL default '',
-  
+
   PRIMARY KEY  (`page`, `module`),
   KEY `module` (`module`),
   KEY `article` (`article`),
-  
+
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `tl_module` (
 
   `bbit_navi_art_enable` char(1) NOT NULL default '',
-  
+
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
