4375fb92ea64877673f0230617698652087664be
David Molineus <david.molineus@netzmacht.de>
2018-12-13 16:41:51 +0100

    Style the mcw.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+ * Backend modules
+ */
+
+if (isset($GLOBALS['BE_MOD']['design']['page']['stylesheet']) && !is_array($GLOBALS['BE_MOD']['design']['page']['css'])) {
+    $GLOBALS['BE_MOD']['design']['page']['css'] = [$GLOBALS['BE_MOD']['design']['page']['css']];
+}
+
+$GLOBALS['BE_MOD']['design']['page']['stylesheet'][] = 'bundles/hofffcontaonavigationarticle/css/backend.css';
+
+/*
+ * Hooks
+ */
+
 $GLOBALS['TL_HOOKS']['bbit_navi_item'][] = [
     \Hofff\Contao\NavigationArticle\EventListener\NavigationArticleListener::class,
     '__invoke',
